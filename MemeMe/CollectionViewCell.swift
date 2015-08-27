ee8ee7c38b06d1198f2ba1203e7247b91881f566
Kyle Mills <kyle@goswim.tv>
2015-08-27 16:20:06 -0400

    initial commit

---
+++ MemeMe/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  MemeMe
+//
+//  Created by Kyle Mills on 8/26/15.
+//  Copyright (c) 2015 Kyle Mills Dev. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ MemeMe/MemeDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MemeDetailViewController.swift
+//  MemeMe
+//
+//  Created by Kyle Mills on 8/26/15.
+//  Copyright (c) 2015 Kyle Mills Dev. All rights reserved.
+//
+
+import UIKit
+
+class MemeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
